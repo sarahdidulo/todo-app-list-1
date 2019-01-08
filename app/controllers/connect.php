15dc676e22daa50e49385a3d8ee417b9671f2e74
Sarah Mae N. Didulo <didulo.sarah@gmail.com>
2019-01-08 15:54:57 +0800

    new update

--- app/controllers/connect.php
+++ app/controllers/connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$username = "root";
-$password = "";
-$dbname = "todo_app_db";
+$host = "db4free.net";
+$username = "sarahdidulo";
+$password = "newpass2019";
+$dbname = "todoapplist";
 
 $conn = mysqli_connect($host, $username, $password, $dbname);
 
